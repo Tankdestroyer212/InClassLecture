cf705d3097dce0c7ed8ff39cf660b88f80b45c6f
Tankdestroyer212 <dhruvkagatimath@gmail.com>
2026-01-13 12:31:22 -0800

    Added arbitrary Sphere Movement(WIP)

--- Assets/SphereMovement.cs
+++ Assets/SphereMovement.cs
@@ -1,11 +1,33 @@
+using System;
+using System.Threading;
 using UnityEngine;
+using UnityEngine.InputSystem.Controls;
 
 public class SphereMovement : MonoBehaviour
 {
+    public float WaitTime = 2;
+    public float Speed;
+    public float Min_X;
+    public float Max_X;
+    public float dir = 1;
+    public float count;
     
-
     void Update()
     {
-        
+        if (transform.position.x > Max_X || transform.position.x < Min_X)
+        {
+            count += Time.deltaTime;
+            if (count >= WaitTime)
+            {
+                dir *= -1;
+                count = 0;
+                transform.position += new Vector3(10 * dir * Speed * Time.deltaTime, 0, 0);
+            }
+        }
+        else
+        {
+            transform.position += new Vector3(dir * Speed * Time.deltaTime, 0, 0);
+        }
+        //Debug.Log(Time.time)
     }
 }
